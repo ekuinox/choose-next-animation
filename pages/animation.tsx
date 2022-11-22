3b6fa8389db8bfedb0f3acf9040d76166d7cef6d
ekuinox <depkey@me.com>
2022-11-23 03:50:55 +0900

    はい

--- pages/animation.tsx
+++ pages/animation.tsx
@@ -1,10 +1,11 @@
 import * as z from "zod";
-import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from "next/link";
 import { useMemo } from "react";
+import { useRouter } from "next/router";
 import { animationCallbackQueryType } from "../lib/annict";
 
-
-export default function Animation() {
+export const Animation: React.FC = () => {
     const { query } = useRouter();
     const animation: z.infer<typeof animationCallbackQueryType> | null = useMemo(() => {
         const animation = animationCallbackQueryType.safeParse(query);
@@ -16,20 +17,47 @@ export default function Animation() {
 
     if (animation == null) {
         return (
-            <div>
-                <a href='/'>back</a>
-            </div>
+            <>
+                <Head>
+                    <title>choose-next-animation | animation</title>
+                </Head>
+                <main>
+                    <div style={{
+                        textAlign: 'center'
+                    }}>
+                        <Link href='/'>top</Link>
+                    </div>
+                </main>
+            </>
         )
     }
 
     const { username, recommendedImageUrl, workId, title, wannaWatchCount } = animation;
 
     return (
-        <div>
-            <a href={`https://annict.com/@${username}`}>{username}</a>さんが次に見るべきアニメは...
-            <img src={recommendedImageUrl} />
-            <a href={`https://annict.com/works/${workId}`}>{title}</a> (1/{wannaWatchCount})
-            <a href='/'>Back</a>
-        </div>
+        <>
+            <Head>
+                <title>{username}は{title}を見たほうが良い</title>
+            </Head>
+            <main>
+                <div style={{
+                    textAlign: 'center'
+                }}>
+                    {recommendedImageUrl.length > 0 && (
+                        <>
+                            <img src={recommendedImageUrl} />
+                            <br />
+                        </>
+                    )}
+                    <Link href={`https://annict.com/@${username}`}>{username}</Link>が次に見るべきアニメは...
+                    <br />
+                    <Link href={`https://annict.com/works/${workId}`}>{title}</Link> (1/{wannaWatchCount})
+                    <br />
+                    <Link href='/'>top</Link>
+                </div>
+            </main>
+        </>
     );
-}
+};
+
+export default Animation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,44 @@
-export default function Home() {
-  return (
-    <a href='/api/login'>login</a>
-  );
+import path from "path";
+import Head from "next/head";
+import Link from "next/link";
+import { readFileSync } from "fs";
+import { GetStaticProps } from "next";
+
+interface HomeProps {
+  repository: string;
+  author: {
+    name: string;
+    email: string;
+    url: string;
+  },
 }
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const json = readFileSync(path.join(process.cwd(), 'package.json'), 'utf-8');
+  const { author, repository: { url } } = JSON.parse(json);
+  return {
+    props: { repository: url, author },
+  };
+};
+
+export const Home: React.FC<HomeProps> = ({ repository, author }) => {
+  return (
+    <>
+      <Head>
+        <title>choose-next-animation</title>
+      </Head>
+      <main>
+        <div style={{
+          textAlign: 'center'
+        }}>
+          <Link href='/api/login'>ログイン</Link>してアニメを選ぶ
+          <div style={{ marginTop: '1vh' }}>
+            <Link href={repository}>Repository</Link> / <Link href={author.url}>{`${author.name} <${author.email}>`}</Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
