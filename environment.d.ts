c8a513da9608b97bb5beea91bd8c2303af7e72b1
ekuinox <depkey@me.com>
2022-11-23 16:27:18 +0900

    これで分けられる？

--- environment.d.ts
+++ environment.d.ts
@@ -2,7 +2,8 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         readonly ANNICT_CLIENT_ID: string;
         readonly ANNICT_CLIENT_SECRET: string;
-        readonly ANNICT_REDIRECT_URL: string;
+        readonly HOST?: string;
+        readonly VERCEL_URL?: string; // https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
         readonly AES_KEY: string;
     }
 }
